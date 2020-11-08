b9bf0622bfbcb202b7ea322f8ba55d3f67f2f76d
mkafonso <makaya2afonso@gmail.com>
2020-11-08 15:20:07 -0300

    user model

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,7 +1,17 @@
 import { Router } from 'express';
 
+import User from './app/models/User';
+
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'Hello world' }));
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Makaya Afonso',
+    email: 'mkafonsos@gmail.com',
+    password_hash: 'dasdsdas',
+  });
+
+  return res.json(user);
+});
 
 export default routes;
